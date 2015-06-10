cbe960809f0bb017e280177a08d1f73a5fdc14ee
bingoohuang <bingoohuang@promote.cache-dns.local>
2015-06-10 20:33:47 +0800

    add sql script

--- src/main/java/com/raiyee/easyhi/apis/merchant/dao/MerchantLookupDao.java
+++ src/main/java/com/raiyee/easyhi/apis/merchant/dao/MerchantLookupDao.java
@@ -7,6 +7,7 @@ import org.n3r.eql.eqler.annotations.Sql;
 public interface MerchantLookupDao {
     // create table resource (mobile varchar(11), tenant_id varchar(20))
     // insert into resource values('18551855185', 't001');
+    // insert into resource values('18551855186', '中文测试');
     @Sql("SELECT TENANT_ID FROM RESOURCE WHERE MOBILE = ##")
     String getTidByMobile(String mobileNo);
 }
