45b851646149a084dc42496c4611885d06d08c71
bingoohuang <bingoohuang@promote.cache-dns.local>
2015-06-10 22:06:05 +0800

    upgrade spring rest client to 0.0.4

--- src/main/java/com/raiyee/easyhi/apis/merchant/controller/DemoController.java
+++ src/main/java/com/raiyee/easyhi/apis/merchant/controller/DemoController.java
@@ -5,7 +5,10 @@ import com.google.common.collect.Maps;
 import com.raiyee.easyhi.apis.merchant.api.YunpianApi;
 import com.raiyee.easyhi.apis.merchant.service.MerchantLookupService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 import java.util.Map;
